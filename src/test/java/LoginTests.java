c16f4d7f92210c20a328072f2f388a94f521e7ea
Spirin <spirin.dmitriy@tech-stack.io>
2023-09-27 17:35:59 +0300

    CORE- add screen shoot for failed tests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,7 +1,9 @@
 import Utils.ScreenShoot;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestListener;
